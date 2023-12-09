50b0fa837fd7d5d6a6142f4309f8dcadcd56ec9f
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-12-09 10:54:22 -0500

    Created react app and setup initial pages

---
+++ react-weather-dashboard/components/fiveDayForecast.display.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function FiveDayForecast() {
+
+    function createForecastCards() {
+
+    }
+
+    return (
+        <div>
+            <p>Five day forecast will go here... eventually</p>
+        </div>
+    )
+}
+
+export default FiveDayForecast;
---
+++ react-weather-dashboard/components/footer.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className='flex justify-between py-5'>
+            <p>Weather App</p>
+            <p>Made with React/Next.js by Ryan Hemlock</p>
+        </footer>
+    )
+}
+
+export default Footer
