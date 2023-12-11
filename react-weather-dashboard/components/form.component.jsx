b4c7940a199eef8d8fef4fee3035626f27fcb94e
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-12-11 08:09:44 -0500

    Created form component and receiving an input

---
+++ react-weather-dashboard/components/form.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react';
+
+
+export default function Form() {
+
+    const [city, setCity] = useState('')
+    const [status, setStatus] = useState('typing')
+
+    function handleInputChange(event) {
+        setCity(event.target.value)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("clicked!");
+        console.log(city);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label for='city'>City:</label>
+
+            <input
+                name="city"
+                type="text"
+                placeholder="Philadelphia"
+                value={city}
+                onChange={handleInputChange} />
+
+            <button
+                disabled={city.length === 0 || status==='submitting'}
+                type="submit"
+            
+            >Search</button>
+        </form>
+    )
+}
